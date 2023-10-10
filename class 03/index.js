031d3854da99ac2196451fb073a83c7b141d4385
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-10-10 08:22:38 -0300

    Create class 03

---
+++ class 03/index.js
@@ -0,0 +1,11 @@
+let num1 = 9.5642;
+
+// let num2 = Math.floor(num1);
+// let num2 = Math.ceil(num1);
+// let num2 = Math.round(num1);
+
+// console.log(Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16));
+// console.log(Math.min(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16));
+
+const numRandom = Math.round(Math.random() * (10 - 1) + 1);
+console.log(numRandom);
