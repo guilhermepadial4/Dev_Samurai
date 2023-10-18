7b25aef1a775f4aa85583b3502c0f78db77af279
Guilherme Padial <guilherme.padial4@gmail.com>
2023-10-18 10:10:13 -0200

    Update index.html and index.js

--- class 05/ex/js/index.js
+++ class 05/ex/js/index.js
@@ -1,3 +1,10 @@
-const nameInput = document.querySelector('name__input');
+const nameInput = document.querySelector('#name__input');
+const btnInput = document.querySelector('#btn__input');
+const lastNameInput = document.querySelector('#lastname__input');
+const weightInput = document.querySelector('#weight__input');
+const heightInput = document.querySelector('#height__input');
 
-console.log(nameInput.value);
+btnInput.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    console.log(nameInput.value, lastNameInput.value, weightInput.value, heightInput.value);
+});
