cf5e8ec743517ec71cd338caf5aa7b6382514177
Guilherme Padial <guilherme.padial4@gmail.com>
2023-10-20 09:43:41 -0300

    Update index.html and index.js

--- class 06/assets/js/index.js
+++ class 06/assets/js/index.js
@@ -2,6 +2,7 @@ const scope = () => {
     const weightInput = document.querySelector('#weight__input');
     const heightInput = document.querySelector('#height__input');
     const btnCalc = document.querySelector('#btn__calc');
+    const resultParag = document.querySelector('#result__parag')
 
     const calcImc = () => {
         let result = weightInput.value / (heightInput.value * heightInput.value);
@@ -9,7 +10,19 @@ const scope = () => {
     }
 
     const showResult = () => {
-        
+        if(calcImc() < 18.5){
+            resultParag.innerText = `O resultado do seu IMC é: ${calcImc()} (Abaixo do peso)`;
+        }else if (calcImc() >= 18.5 && calcImc() <= 24.9) {
+            resultParag.innerText = `O resultado do seu IMC é: ${calcImc()} (Peso normal)`;
+        }else if (calcImc() >= 25 && calcImc() <= 29.9) {
+            resultParag.innerText = `O resultado do seu IMC é: ${calcImc()} (Acima do peso)`;
+        }else if (calcImc() >= 30 && calcImc() <= 34.9) {
+            resultParag.innerText = `O resultado do seu IMC é: ${calcImc()} (Obesidade grau 1)`;
+        }else if (calcImc() >= 35 && calcImc() <= 39.9) {
+            resultParag.innerText = `O resultado do seu IMC é: ${calcImc()} (Obesidade grau 2)`;
+        }else if (calcImc() > 40) {
+            resultParag.innerText = `O resultado do seu IMC é: ${calcImc()} (Obesidade grau 3)`;
+        }
     }
 
     btnCalc.addEventListener('click', (evt) => {
