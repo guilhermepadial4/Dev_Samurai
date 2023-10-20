6b3093dbbb2417fab6bb73d8f0f24dfc759e777f
Guilherme Padial <guilherme.padial4@gmail.com>
2023-10-20 09:15:41 -0300

    Update index.js

--- class 06/assets/js/index.js
+++ class 06/assets/js/index.js
@@ -4,14 +4,18 @@ const scope = () => {
     const btnCalc = document.querySelector('#btn__calc');
 
     const calcImc = () => {
-        let result = Number(weightInput / (heightInput * heightInput));
-        console.log(result);
+        let result = weightInput.value / (heightInput.value * heightInput.value);
+        return result.toFixed(1);
+    }
+
+    const showResult = () => {
+        
     }
 
     btnCalc.addEventListener('click', (evt) => {
         evt.preventDefault();
-        console.log(typeof(Number(weightInput)));
-        // calcImc();
+        console.log(calcImc());
+        showResult();
     });
 }
 
