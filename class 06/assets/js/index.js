af9ebf59da66107bb3bb3dd2778d8790f6236704
Guilherme Padial <guilherme.padial4@gmail.com>
2023-10-20 12:00:55 -0300

    Update index.js

--- class 06/assets/js/index.js
+++ class 06/assets/js/index.js
@@ -37,7 +37,7 @@ const scope = () => {
         evt.preventDefault();
         // console.log(calcImc());
         showResult();
-        validationInput();
+        // validationInput();
     });
 }
 
