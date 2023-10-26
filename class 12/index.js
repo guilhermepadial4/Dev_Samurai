ed4e90793b88e60f42d8a00dbfa74f1c96b6afb9
Guilherme Padial <guilherme.padial4@gmail.com>
2023-10-26 10:24:25 -0300

    Create class 12

---
+++ class 12/index.js
@@ -0,0 +1,10 @@
+// for(let i = 1; i <= 10; i++) {
+//     const par = i % 2 === 0 ? 'par' : 'impar';
+//     console.log(i, par);
+// }
+
+const fruit = ['Apple', 'Melon', 'Orange'];
+
+for(let i = 0; i < fruit.length; i++) {
+    console.log(fruit[i]);
+}
