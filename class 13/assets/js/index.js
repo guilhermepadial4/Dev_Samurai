949125fd2277cce4f5c688cf6b16d9ccb19db537
Guilherme Padial <guilherme.padial4@gmail.com>
2023-10-26 11:35:31 -0300

    Update index.js

--- class 13/assets/js/index.js
+++ class 13/assets/js/index.js
@@ -1,8 +1,4 @@
 const mainScope = () => {
-    const container = document.querySelector('#section__container');
-
-    const div = document.createElement('div');
-    container.appendChild(div);
 
     const elements = [
         {tag: 'p', text: 'word 1'},
@@ -10,6 +6,18 @@ const mainScope = () => {
         {tag: 'footer', text: 'word 3'},
         {tag: 'section', text: 'word 4'},
     ]
+
+    const container = document.querySelector('#section__container');
+    const div = document.createElement('div');
+
+
+    for (let i = 0; i < elements.length; i++) {
+        let { tag, text } = elements[i];
+        let createTag = document.createElement(tag);
+        createTag.innerHTML = text;
+        div.appendChild(createTag);
+    }
+    container.appendChild(div);
 }
 
 mainScope();
