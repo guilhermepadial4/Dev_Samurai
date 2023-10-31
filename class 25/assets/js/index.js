8d4960324e86bc46e2bbc585c91de8ef8d1d5916
Guilherme Padial <guilherme.padial4@gmail.com>
2023-10-31 11:36:26 -0300

    Update index.htm and index.js

--- class 25/assets/js/index.js
+++ class 25/assets/js/index.js
@@ -1,1 +1,7 @@
+const mainScope = () => {
+    const inputAddTask = document.querySelector('#input__add_task');
+    const btnAddTask = document.querySelector('#btn__add_task');
+    const tasks = document.querySelector('#tasks');
+}
 
+mainScope();
