c122e4a5b6f03754638dae76fbcc7bb7be3ae97c
Guilherme Padial <guilherme.padial4@gmail.com>
2023-11-01 10:32:14 -0300

    Update index.js

--- class 25/assets/js/index.js
+++ class 25/assets/js/index.js
@@ -2,6 +2,47 @@ const mainScope = () => {
     const inputAddTask = document.querySelector('#input__add_task');
     const btnAddTask = document.querySelector('#btn__add_task');
     const tasks = document.querySelector('#tasks');
+
+    const createLi = () => {
+        const li = document.createElement('li');
+        return li;
+    }
+
+    const clearInput = () => {
+        inputAddTask.value = '';
+        inputAddTask.focus();
+    }
+
+    const createBtnClear = (li) => {
+        li.innerText += ' ';
+        const btnClear = document.createElement('button');
+        btnClear.innerText = 'Apagar';
+        btnClear.setAttribute('class', 'clear');
+        li.appendChild(btnClear);
+    }
+
+    const createText = (text) => {
+        const li = createLi();
+        li.innerText += text;        
+        tasks.appendChild(li);
+        clearInput();
+        createBtnClear(li);
+    } 
+
+    inputAddTask.addEventListener('keypress', (evt) => {
+        if (evt.keyCode === 13) {
+            if (!inputAddTask.value) return;
+            createText(inputAddTask.value);
+            
+        }
+    });
+
+    btnAddTask.addEventListener('click', (evt) => {
+        if (!inputAddTask.value) return;
+        createText(inputAddTask.value);
+        
+    });
+
 }
 
 mainScope();
