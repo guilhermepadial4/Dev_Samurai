a7f3cb58ba1c1181208a7a03d156adf8567d8e40
shaolin96 <guilherme.padial4@gmail.com>
2023-11-14 09:27:42 -0300

    Update index.js of class 39

--- class 39/index.js
+++ class 39/index.js
@@ -1,13 +1,14 @@
 function Pessoa(nome, sobrenome) {
     this.nome = nome;
     this.sobrenome = sobrenome;
-    this.nomeCompleto = () => {
-        `${this.nome} ${this.sobrenome}`;
-    }
+}
+
+Pessoa.prototype.nomeCompleto = function() {
+    return `${this.nome} ${this.sobrenome}`;
 }
 
 const p1 = new Pessoa ('Guilherme', 'Padial');
-const p2 = new Pessoa ('Rafael', 'Padial');
+const data = new Date();
 
 console.dir(p1);
-console.dir(p2);
+console.dir(data);
