b8c1070b5c766e508217a48399a0132c5b0d2120
shaolin96 <guilherme.padial4@gmail.com>
2023-11-28 09:25:49 -0300

    Update index.js of class 50

--- class 50/index.js
+++ class 50/index.js
@@ -1,50 +1,50 @@
-class validadorCpf {
-    constructor(cpfEnviado) {
-        Object.defineProperty(this, 'cpfLimpo', {
+class validatorCpf {
+    constructor(cpfSubmit) {
+        Object.defineProperty(this, 'cpfClean', {
             writable: false,
             enumerable: false,
             configurable: false,
-            value: cpfEnviado.replace(/\D+/g, '')
+            value: cpfSubmit.replace(/\D+/g, '')
         });
     }
 
-    eSequencia() {
-        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+    isSequence() {
+        return this.cpfClean.charAt(0).repeat(11) === this.cpfClean;
     }
 
-    geraNovoCpf() {
-        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
-        const digito1 = this.geraDigito(cpfSemDigitos);
-        const digito2 = this.geraDigito(cpfSemDigitos + digito1);
-        this.novoCpf = cpfSemDigitos + digito1 + digito2;
+    generateNewCpf() {
+        const cpfWithoutDigits = this.cpfClean.slice(0, -2);
+        const digit1 = this.generateDigit(cpfWithoutDigits);
+        const digit2 = this.generateDigit(cpfWithoutDigits + digit1);
+        this.newCpf = cpfWithoutDigits + digit1 + digit2;
     }
 
-    geraDigito(cpfSemDigitos) {
+    generateDigit(cpfWithoutDigits) {
         let total = 0;
-        let reverso = cpfSemDigitos.length + 1;
+        let reverso = cpfWithoutDigits.length + 1;
 
-        for (let stringNumerica of cpfSemDigitos) {
-            total += reverso * Number(stringNumerica);
+        for (let stringNumber of cpfWithoutDigits) {
+            total += reverso * Number(stringNumber);
             reverso--
         }
 
-        const digito = 11 - (total % 11);
-        return digito <= 9 ? String(digito) : '0';
+        const digit = 11 - (total % 11);
+        return digit <= 9 ? String(digit) : '0';
     }
 
-    valida() {
-        if (!this.cpfLimpo) return false;
-        if (typeof this.cpfLimpo !== 'string') return false;
-        if (this.cpfLimpo.length !== 11) return false
-        if (this.eSequencia()) return false;
-        this.geraNovoCpf();
+    valid() {
+        if (!this.cpfClean) return false;
+        if (typeof this.cpfClean !== 'string') return false;
+        if (this.cpfClean.length !== 11) return false
+        if (this.isSequence()) return false;
+        this.generateNewCpf();
 
-        return this.novoCpf === this.cpfLimpo;
+        return this.newCpf === this.cpfClean;
     }
 }
 
-const cpf1 = new validadorCpf('008.532.998-38');
-if (cpf1.valida()) {
+const cpf1 = new validatorCpf('761.037.070-30');
+if (cpf1.valid()) {
     console.log('CPF válido');
 } else {
     console.log('CPF inválido');
