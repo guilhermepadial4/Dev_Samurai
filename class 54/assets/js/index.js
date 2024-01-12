3056e59f10ff25b210cd4ec705aab05ed8f232b4
Guilherme Padial <guilherme.padial4@gmail.com>
2024-01-12 11:08:39 -0300

    Update the class 54

--- class 54/assets/js/index.js
+++ class 54/assets/js/index.js
@@ -1,13 +1,46 @@
 const request = (obj) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(obj.method, obj.url, true);
-    xhr.send();
-
-    xhr.addEventListener('load', (e) => {
-        if(xhr.status >= 200 && xhr.status < 300) {
-            obj.sucess(xhr.responseText);
-        }else {
-            obj.error(xhr.status);
-        }
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(obj.method, obj.url, true);
+        xhr.send();
+    
+        xhr.addEventListener('load', () => {
+            if(xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseText)
+            }else {
+                reject(xhr.statusText);
+            }
+        })
     })
+}
+
+document.addEventListener('click', (event) => {
+    const el = event.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        event.preventDefault();
+        loadPage(el);
+    }
+})
+
+const loadPage = async (el) => {
+    const href = el.getAttribute('href');
+    
+    const objConfig = {
+        method: 'GET',
+        url: href
+    };
+
+    try {
+        const response =  await request(objConfig);
+        loadResult(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const loadResult = (response) => {
+    const result = document.querySelector('.result');
+    result.innerHTML = response;
 }
